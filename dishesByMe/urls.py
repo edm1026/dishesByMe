3f7c6257e9868cf5c2ad49c0afd39792287fffbd
edanim <edanimartinez711@gmail.com>
2017-05-02 21:40:23 -0500

    urls.py square bracket homeurl edit

--- dishesByMe/urls.py
+++ dishesByMe/urls.py
@@ -23,7 +23,7 @@ from django.views.generic.base import RedirectView
 urlpatterns = [
     url(r'^$', RedirectView.as_view(url='/home/')),
     url(r'^admin/', admin.site.urls),
-#    url(r'^home/', include('home.urls')),
+    url(r'^home/', include('home.urls')),
 ]
 
 """
